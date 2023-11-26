b7206598d12fa05931710b05fd8ca9a5296b58d8
maixi <maixiaochai@outlook.com>
2023-11-26 18:37:18 +0800

    利用中断+巧妙的计数器的设置，实现了植物干燥时，自动浇水指定秒数的功能

--- projects/01.waterTheFlowerAuto/waterTheFlowerAuto/waterTheFlowerAuto.ino
+++ projects/01.waterTheFlowerAuto/waterTheFlowerAuto/waterTheFlowerAuto.ino
@@ -1,12 +1,14 @@
-# include "TimerOne.h"
+#include "TimerOne.h"
 #define PIN_AO 0
 #define PIN_DO 3
 
 long check_interval_seconds = 1 * 1000000;
-long water_seconds = 20 * 1000000;
+int min_humidity = 450;
 int pin_relay = 2;
 int sensor_level = LOW;
 int sensor_number;
+int counter = -1;
+int water_seconds = 20;
 
 void setup() {
   pinMode(PIN_AO, INPUT);
@@ -21,19 +23,27 @@ void setup() {
 void callback() {
   sensor_number = analogRead(PIN_AO);
   sensor_level = digitalRead(PIN_DO);
-  if (sensor_level == HIGH) {
-    if (sensor_number >= 500) {
-      digitalWrite(pin_relay, HIGH);
-      Timer1.initialize(water_seconds);
 
-    } else {
-        digitalWrite(pin_relay, LOW);
-        Timer1.initialize(check_interval_seconds);
-      }
-      Serial.print("AO=");
-      Serial.print(sensor_number);
-      Serial.print(", DO=");
-      Serial.println(sensor_level);
+  if (sensor_level == HIGH) {
+    if (sensor_number >= min_humidity) {
+      counter=0;
+      digitalWrite(pin_relay, HIGH);
+    };
+
+    if (counter >= 0) {
+      counter++;
+    }
+
+    if (counter >= water_seconds) {
+      digitalWrite(pin_relay, LOW);
+      counter= -1;
+    }
+    Serial.print("AO=");
+    Serial.print(sensor_number);
+    Serial.print(", DO=");
+    Serial.print(sensor_level);
+    Serial.print(", counter=");
+    Serial.println(counter);
   };
 }
 
